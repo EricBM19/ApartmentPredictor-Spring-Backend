6e37ec232bfe51b1488a9715015f06aacdc693e0
EricBM19 <ericbm19@gmail.com>
2026-02-12 16:01:05 +0100

    First commit.

---
+++ apartmentPredictor_Backend/src/main/java/com/ebm/apartmentPredictor_Backend/ApartmentPredictorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ebm.apartmentPredictor_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentPredictorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentPredictorBackendApplication.class, args);
+	}
+
+}
---
+++ apartmentPredictor_Backend/src/test/java/com/ebm/apartmentPredictor_Backend/ApartmentPredictorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebm.apartmentPredictor_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentPredictorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
